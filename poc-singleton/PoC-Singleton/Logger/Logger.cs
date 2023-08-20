6cde2b231184cfc83e33ea7af57d02edab9aeccd
abelmaro <contactomaronas@gmail.com>
2023-08-20 00:09:54 -0300

    Add singleton design pattern project

---
+++ poc-singleton/PoC-Singleton/Logger/Logger.cs
@@ -0,0 +1,21 @@
+﻿namespace PoC_Singleton.Logger
+{
+    public class LoggerI
+    {
+        private static LoggerI? instance;
+        private LoggerI() { }
+
+        public static LoggerI Instance
+        {
+            get
+            {
+                return instance ??= new LoggerI();
+            }
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now}] {message}");
+        }
+    }
+}
---
+++ poc-singleton/PoC-Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using PoC_Singleton.Logger;
+
+namespace PoC_Singleton
+{
+    public class Program
+    {
+        protected Program() { }
+        static void Main()
+        {
+            LoggerI logger = LoggerI.Instance;
+
+            logger.Log("Starting application...");
+
+            for (int i = 0; i < 5; i++)
+                logger.Log($"Logging event {i}");
+
+            logger.Log("Ending application...");
+        }
+    }
+}
